f4b7761749b320e06e3177466d8d9b7c2ca002f2
Ben Wood <bwood@wheelercat.com>
2018-07-13 14:20:51 -0600

    Adds Edit / Delete actions
    Adds the edit url routing to the method showEditForm. Adds the delete url routing to the method deleteBook.

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -44,24 +44,29 @@ public class ControllerServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException  {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
 		String action = request.getPathInfo();
 
 		try {
 			switch(action) {
 				case "/admin":
 					 showBookAdmin(request, response);
-           break;
-			  case "/new":
+					 break;
+				case "/new":
 					showNewForm(request, response);
-          break;
+					break;
 				case "/insert":
 					insertBook(request, response);
-          break;
-        default:
+					break;
+				case "/delete":
+					deleteBook(request, response);
+					break;
+				case "/edit":
+					showEditForm(request, response);
+					break;
+				default:
 				   listBooks(request, response);
-           break;
+				   break;
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -69,8 +74,7 @@ public class ControllerServlet extends HttpServlet {
 		}
 	}
 
-	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
-			throws ClassNotFoundException, SQLException, ServletException, IOException {
+	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response) throws ClassNotFoundException, SQLException, ServletException, IOException {
 		ArrayList<Book> books_list = bookDAO.listAllBooks();
 
 		request.setAttribute("books", books_list);
@@ -78,8 +82,7 @@ public class ControllerServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	private void listBooks(HttpServletRequest request, HttpServletResponse response)
-			throws ClassNotFoundException, SQLException, ServletException, IOException {
+	private void listBooks(HttpServletRequest request, HttpServletResponse response) throws ClassNotFoundException, SQLException, ServletException, IOException {
 		ArrayList<Book> books_list = bookDAO.listAllBooks();
 
 		request.setAttribute("books", books_list);
@@ -87,14 +90,12 @@ public class ControllerServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
 		dispatcher.forward(request, response);
 	}
 
-	private void insertBook(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException, ClassNotFoundException, SQLException {
+	private void insertBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException, SQLException {
 		String title = request.getParameter("booktitle");
 		String author = request.getParameter("bookauthor");
 		String priceString = request.getParameter("bookprice");
@@ -105,6 +106,14 @@ public class ControllerServlet extends HttpServlet {
 		response.sendRedirect("list");
 	}
 
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		
+	}	
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		
+	}		
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
